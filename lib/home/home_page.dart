2331d3761e76bad2c8bcc83f3918245f2286cb04
KavishkaSasindu <sasindu0215@gmail.com>
2024-04-18 09:54:08 +0530

    updated bys kdev

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/widgets/app_bar/app_bar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,48 +8,9 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Container(
-          width: 20,
-          height: 20,
-          decoration: BoxDecoration(
-              color: const Color(0xff9E00FF),
-              borderRadius: BorderRadius.circular(1000)),
-          child: const Center(
-            child: Icon(
-              Icons.location_on,
-              color: Colors.white,
-            ),
-          ),
-        ),
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Delivery Address",
-              style: TextStyle(
-                  fontSize: 18,
-                  color: const Color(0xff3B3636).withOpacity(0.47)),
-            ),
-            const Text(
-              "92 High Street,London",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            )
-          ],
-        ),
-        actions: [
-          Container(
-            width: 50,
-            height: 50,
-            decoration: BoxDecoration(
-                color: Colors.amber, borderRadius: BorderRadius.circular(1000)),
-            child: const Center(
-              child: Icon(
-                Icons.notification_add,
-                color: Colors.white,
-              ),
-            ),
-          )
-        ],
+        leading: appLeadingWidget,
+        title: appTitleWidget,
+        actions: [appActionOne],
       ),
     );
   }
---
+++ lib/widgets/app_bar/app_bar.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+Widget appLeadingWidget = Padding(
+  padding: const EdgeInsets.all(5.0),
+  child: Container(
+    width: 20,
+    height: 20,
+    decoration: BoxDecoration(
+        color: const Color(0xff9E00FF),
+        borderRadius: BorderRadius.circular(1000)),
+    child: const Center(
+      child: Icon(
+        Icons.location_on,
+        color: Colors.white,
+      ),
+    ),
+  ),
+);
+
+Widget appTitleWidget = Column(
+  crossAxisAlignment: CrossAxisAlignment.start,
+  children: [
+    Text(
+      "Delivery Address",
+      style: TextStyle(
+          fontSize: 18, color: const Color(0xff3B3636).withOpacity(0.47)),
+    ),
+    const Text(
+      "92 High Street,London",
+      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    )
+  ],
+);
+
+Widget appActionOne = Padding(
+  padding: const EdgeInsets.all(5.0),
+  child: Container(
+    width: 50,
+    height: 50,
+    decoration: BoxDecoration(
+        color: Colors.amber, borderRadius: BorderRadius.circular(1000)),
+    child: const Center(
+      child: Icon(
+        Icons.card_travel,
+        color: Colors.white,
+      ),
+    ),
+  ),
+);
